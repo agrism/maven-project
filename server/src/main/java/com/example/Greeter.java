7728bd433f99c7ff010ef94f692e4690e9337e09
Agris Markus <7924@inbox.lv>
2018-08-25 00:57:33 +0300

    fix stylecheck

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,10 @@ public class Greeter {
 
   }
 
-	/**
-	* @param someone the nme of person
-	* @return greeting string
-	*/
+  /**
+  * @param someone the nme of person
+  * @return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
